f86966ea6cafb7a26e6616be721d7ee4cc3a08c8
xiaoye <1844881928@qq.com>
2019-03-12 17:54:35 +0800

    redis 配置

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello World !";
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping("/getHello")
+    public String getHello(){
+        return redisService.get("hello").toString();
+    }
+
+    @RequestMapping("/setHello")
+    public void setHello(){
+        redisService.set("hello","HelloWorld");
     }
 }
